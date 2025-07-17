3ebc498c66bb4ec26a8cca40bb69ed034b4bf868
@MontanaCode2077 <alfredoaguipuga91@gmail.com>
2025-07-17 16:35:54 -0600

    Factor 200

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -6,5 +6,10 @@ b = 30
 c = 30 * 10 * 5
 print ('resultado=', c)
 
-d = c * 100
+## Caambiado el factor 102 según su requerimiento del negocio
+## Cambiado el factor 110 según 110 Junta de Control
+## Cambio a factor 200 por Directorio
+
+
+d = c * 200
 print ('resultado total =', d)
